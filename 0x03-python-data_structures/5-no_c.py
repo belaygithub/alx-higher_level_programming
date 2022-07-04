a4354118fdbf104e5fd31c1af725b1984f6e0a95
belaygit <belayeshetu2014@gmail.com>
2022-07-04 05:42:06 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    listlength = len(my_list) - 1
-    if (idx < 0 or idx > listlength):
-        return (my_list)
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return (new_list)
+def no_c(my_string):
+    listofchars = list(my_string)
+    for char in listofchars:
+        if char == 'c' or char == 'C':
+            listofchars.remove(char)
+    return("".join(listofchars))
