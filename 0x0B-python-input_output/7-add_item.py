5b668b4f8aaf37051cfae2ce93deac0a8cb7a644
belaygit <belayeshetu2014@gmail.com>
2022-08-02 11:26:30 -0700

    fixed 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """ adds all arguments to a list, and then save them to a file """
 import sys
 import os
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 filename = 'add_item.json'
