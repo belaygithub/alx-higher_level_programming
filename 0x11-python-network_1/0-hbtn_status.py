4ca020bd8a618d65c08a87c2ba50e5323a8eb13e
belaygit <belayeshetu2014@gmail.com>
2022-10-21 08:44:34 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
+""" Fetching URLs with urllib """
+import urllib.request
+
 if __name__ == "__main__":
-    import urllib.request as request
-    with request.urlopen('https://intranet.hbtn.io/status') as r
-        html = r.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+              .format(type(html), html, html.decode('utf-8')))
